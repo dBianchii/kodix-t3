ed538cc37ca31565e192bc31ac54ea5dab69abda
gdbianchii <gdbianchii@gmail.com>
2022-11-29 18:51:32 -0300

    Get techs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
 	const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+	//const { data } = trpc.example.getHappyPeople.useQuery();
+	const { data } = trpc.example.getTechs.useQuery();
 
 	return (
 		<>
@@ -17,6 +19,7 @@ const Home: NextPage = () => {
 			</Head>
 			<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
 				<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+
 					<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
 						Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
 					</h1>
@@ -45,6 +48,9 @@ const Home: NextPage = () => {
 					<div className="flex flex-col items-center gap-2">
 						<p className="text-2xl text-white">
 							{hello.data ? hello.data.greeting : "Loading tRPC query..."}
+						</p>
+						<p className="text-2xl text-white">
+							{data?.map(tech => tech.name).join(', ')}
 						</p>
 						<AuthShowcase />
 					</div>
--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -1,16 +1,20 @@
 import { z } from "zod";
 
-import { router, publicProcedure } from "../trpc";
+import { router, publicProcedure, protectedProcedure } from "../trpc";
 
 export const exampleRouter = router({
-  hello: publicProcedure
-    .input(z.object({ text: z.string().nullish() }).nullish())
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
-    }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+	hello: publicProcedure
+		.input(z.object({ text: z.string().nullish() }).nullish())
+		.query(({ input }) => {
+			return {
+				greeting: `Hello ${input?.text ?? "world"}`,
+				date: new Date()
+			};
+		}),
+	getAll: publicProcedure.query(({ ctx }) => {
+		return ctx.prisma.example.findMany();
+	}),
+	getTechs: protectedProcedure.query(({ ctx }) => {
+		return ctx.prisma.technology.findMany()
+	})
 });
