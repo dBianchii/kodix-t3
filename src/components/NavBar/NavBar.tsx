16f594c93c52806c14266387c8c363853486855a
gdbianchii <gdbianchii@gmail.com>
2022-12-15 12:50:01 -0300

    Some login Fixes

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -16,6 +16,7 @@ import {
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const solutions = [
   {
@@ -243,9 +244,9 @@ export default function Example() {
             </Popover>
           </Popover.Group>
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-						<a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+						<Link href="/signIn" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
 							Sign in
-						</a>
+						</Link>
 						<a
 							href="#"
 							className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
