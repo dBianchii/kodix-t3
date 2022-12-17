ae15f3d90a48496d42e260e2bddd9ea122069adc
dbianchii <gdbianchii@gmail.com>
2022-12-17 10:23:21 -0300

    Navbar url

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -16,6 +16,7 @@ import {
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const solutions = [
   {
@@ -88,7 +89,7 @@ export default function Example() {
             <a href="#">
               <span className="sr-only">Your Company</span>
               
-							<h3 className="text-lg text-blue-500">Kodix</h3>
+				<h3 className="text-lg text-blue-500">Kodix</h3>
             </a>
           </div>
           <div className="-my-2 -mr-2 md:hidden">
@@ -243,15 +244,15 @@ export default function Example() {
             </Popover>
           </Popover.Group>
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-						<a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-							Sign in
-						</a>
-						<a
-							href="#"
-							className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-						>
-							Sign up
-						</a>
+			<Link href="/login" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+				Sign in
+			</Link>
+			<Link
+				href="/login"
+				className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+			>
+				Sign up
+			</Link>
 
           </div>
         </div>
