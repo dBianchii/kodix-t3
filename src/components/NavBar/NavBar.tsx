fc65cd11ae08c0bc09edf18c0d0702252d7e994d
dbianchii <gdbianchii@gmail.com>
2022-12-17 10:46:09 -0300

    Better navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -13,11 +13,14 @@ import {
   ShieldCheckIcon,
   Squares2X2Icon,
   XMarkIcon,
+  BriefcaseIcon,
+  ArrowLeftOnRectangleIcon,
+  Cog6ToothIcon
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
 import Link from 'next/link'
-
+import { useSession } from 'next-auth/react'
 const solutions = [
   {
     name: 'Analytics',
@@ -48,6 +51,18 @@ const solutions = [
 const callsToAction = [
   { name: 'Watch Demo', href: '#', icon: PlayIcon },
   { name: 'Contact Sales', href: '#', icon: PhoneIcon },
+]
+const profilePic = [
+  {
+    name: 'Your workspaces',
+    description: 'Create, and manage your workspaces',
+    href: '/newWorkspace',
+    icon: BriefcaseIcon,
+  }
+]
+const callsToActionProfilePic = [
+  { name: 'Settings', href: '#', icon: Cog6ToothIcon },
+  { name: 'Log Out', href: '#', icon: ArrowLeftOnRectangleIcon },
 ]
 const resources = [
   {
@@ -81,6 +96,8 @@ function classNames(...classes: string[] ) {
 }
 
 export default function Example() {
+	const { data: session } = useSession()
+
   return (
     <Popover className="relative bg-white">
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
@@ -243,16 +260,83 @@ export default function Example() {
             </Popover>
           </Popover.Group>
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-			<Link href="/login" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-				Sign in
-			</Link>
-			<Link
-				href="/login"
-				className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-			>
-				Sign up
-			</Link>
-
+				    {
+				    	session?.user?.image ? (
+				    		<div>
+				    			<Popover className="relative">
+                    {({ open }) => (
+                      <>
+                        <Popover.Button
+                          className={classNames(
+                            open ? 'text-gray-900' : 'text-gray-500',
+                            'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                          )}
+                        >
+                          <Image className="w-10 h-10 rounded-full" src={session?.user?.image?.toString()} alt="Rounded avatar" width={40} height={40}/>
+                          
+                        </Popover.Button>
+                            
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-200"
+                          enterFrom="opacity-0 translate-y-1"
+                          enterTo="opacity-100 translate-y-0"
+                          leave="transition ease-in duration-150"
+                          leaveFrom="opacity-100 translate-y-0"
+                          leaveTo="opacity-0 translate-y-1"
+                        >
+                          <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
+                            <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                              <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                                {profilePic.map((item) => (
+                                  <a
+                                    key={item.name}
+                                    href={item.href}
+                                    className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                                  >
+                                    <item.icon className="h-6 w-6 flex-shrink-0 text-indigo-600" aria-hidden="true" />
+                                    <div className="ml-4">
+                                      <p className="text-base font-medium text-gray-900">{item.name}</p>
+                                      <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+                                    </div>
+                                  </a>
+                                ))}
+                              </div>
+                              <div className="space-y-6 bg-gray-50 px-5 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
+                                {callsToActionProfilePic.map((item) => (
+                                  <div key={item.name} className="flow-root">
+                                    <a
+                                      href={item.href}
+                                      className="-m-3 flex items-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100"
+                                    >
+                                      <item.icon className="h-6 w-6 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                      <span className="ml-3">{item.name}</span>
+                                    </a>
+                                  </div>
+                                ))}
+                              </div>
+                            </div>
+                          </Popover.Panel>
+                        </Transition>
+                      </>
+                    )}
+                  </Popover>
+				    		</div>
+				    	) : (
+				    		<div>
+				    			<Link href="/signIn" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+				    			Sign in
+				    			</Link>
+				    			<Link
+				    				href="/signIn"
+				    				className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+				    			>
+				    				Sign up
+				    			</Link>
+				    		</div>
+    
+				    	)
+				    }
           </div>
         </div>
       </div>
