60e2066af82a5e99e3ef3b19ddeae2811a0aa0eb
gdbianchii <gdbianchii@gmail.com>
2022-12-21 11:18:13 -0300

    Email provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
+import EmailProvider from "next-auth/providers/email";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
@@ -21,11 +22,22 @@ export const authOptions: NextAuthOptions = {
 		GoogleProvider({
 			clientId: env.GOOGLE_CLIENT_ID,
 			clientSecret: env.GOOGLE_CLIENT_SECRET,
+		}),
+		EmailProvider({
+			server: {
+				host: env.EMAIL_SERVER_HOST,
+				port: env.EMAIL_SERVER_PORT,
+				auth: {
+					user: env.EMAIL_SERVER_USER,
+					pass: env.EMAIL_SERVER_PASSWORD
+				}
+			},
+			from: env.EMAIL_FROM
 		})
 	],
 	secret: env.NEXTAUTH_SECRET,
 	pages: {
-		//signIn: '/auth/login',
+		signIn: '/auth/signIn',
 		//signOut: '/auth/signout',
 		//error: '/auth/error', // Error code passed in query string as ?error=
 		//verifyRequest: '/auth/verify-request', // (used for check email message)
--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -1,9 +1,21 @@
-import { signIn, getProviders, type LiteralUnion, type ClientSafeProvider, useSession } from "next-auth/react";
+import { signIn, getProviders, type LiteralUnion, type ClientSafeProvider, useSession, type SignInOptions } from "next-auth/react";
 import type { BuiltInProviderType } from "next-auth/providers";
 import { FcGoogle } from 'react-icons/fc';
 import { type FC, useEffect, useState } from "react";
 
 const Login: FC = () => {	
+	const signInOptions: SignInOptions = {
+		email: "",
+		callbackUrl: '/'
+	}
+	const [email, setEmail] = useState(signInOptions)
+	const handleEmail = (emailString: string) => {
+		const signInOptions: SignInOptions = {
+			email: emailString,
+			callbackUrl: '/'
+		}
+		setEmail(signInOptions)
+	};
 
 	const [providers, setproviders] = useState<Record<
 		LiteralUnion<BuiltInProviderType, string>,
@@ -33,7 +45,7 @@ const Login: FC = () => {
 					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 
 						
-						<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+						<h1 className="text-xl font-bold text-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
 							Sign in to your account
 						</h1>
 						<form className="space-y-4 md:space-y-6" action="#">
@@ -41,32 +53,18 @@ const Login: FC = () => {
 							<>
 								<div>
 									<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-									<input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"/>
+									<input type="email" name="email" id="email" onChange={(e) => handleEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"/>
 								</div>
-								<div>
-									<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-									<input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-								</div>
-								<div className="flex items-center justify-between">
-									<div className="flex items-start">
-										<div className="flex items-center h-5">
-										<input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"/>
-										</div>
-										<div className="ml-3 text-sm">
-										<label className="text-gray-500 dark:text-gray-300">Remember me</label>
-										</div>
-									</div>
-									<a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-								</div>
-								<button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-								<p className="text-sm font-light text-gray-500 dark:text-gray-400">
-									Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-								</p>
+								<button type="button" onClick={() => signIn(providers.email.id, email)} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+							
+								<hr />
+
 							</>
 						)}
 						{providers?.google && (
-							<button type="button" onClick={() => signIn(providers.google.id, { callbackUrl: '/' })} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"><FcGoogle className=""/> Login With Google</button>
+							<button type="button" onClick={() => signIn(providers.google.id, { callbackUrl: '/' })} className="text-white bg-gray-600 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"><FcGoogle className=""/> Login With Google</button>
 						)}
+						
 							
 						</form>
 					</div>
@@ -76,7 +74,6 @@ const Login: FC = () => {
 	)
 }
 export default Login
-
 
 export async function getServerSideProps() {
 	const providers = await getProviders()
