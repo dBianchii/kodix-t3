43499f354ae1cc3a5b375baf4d26b0d76f186f67
dbianchii <gdbianchii@gmail.com>
2022-12-31 18:30:36 -0300

    fix

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -48,7 +48,7 @@ export const authOptions: NextAuthOptions = {
     //signOut: '/auth/signout',
     //error: '/auth/error', // Error code passed in query string as ?error=
     //verifyRequest: '/auth/verify-request', // (used for check email message)
-    newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+    //newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
 };
 
