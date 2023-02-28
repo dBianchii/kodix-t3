f4986445ee19970abcfb92627500c6ac4a65d7dc
gdbianchii <gdbianchii@gmail.com>
2023-02-27 22:34:09 -0300

    fix

--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -1,39 +1,26 @@
-import { useSession } from "next-auth/react";
 import type { App } from "@prisma/client";
 import AppComponent from "../components/App/App";
-import type { AppWithInstalled } from "../server/api/routers/apps";
 import { api } from "../utils/api";
-import { InferGetServerSidePropsType } from "next";
 
-export const getServerSideProps = () => {
-  let result: App[] | AppWithInstalled[] = [];
-  if (!session) {
-    const { data } = api.app.getAll.useQuery();
-    if (data) result = data;
-  } else {
-    const { data } = api.app.getAllWithInstalled.useQuery();
-    if (data) result = data;
+export default function Marketplace() {
+  const data = api.app.getAll.useQuery();
+  if (data.isLoading) {
+    return <div>Loading...</div>;
   }
 
-  return {
-    props: {
-      result,
-    },
-  };
-};
+  if (!data.data) {
+    return <div>Error</div>;
+  }
 
-export default function Marketplace(
-  props: InferGetServerSidePropsType<typeof getServerSideProps>
-) {
   return (
-    <div className="flex min-h-screen flex-row bg-gray-800 bg-gradient-to-b">
+    <div className="flex min-h-screen flex-row bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       <div className="mx-10 my-4">
-        {props.result.map((app) => (
+        {data.data.map((app: App) => (
           <div key={app.id}>
             <AppComponent
               appName={app.name}
               appDescription={app.description}
-              installed={"installed" in app ? app.installed : false}
+              installed={false}
             />
           </div>
         ))}
